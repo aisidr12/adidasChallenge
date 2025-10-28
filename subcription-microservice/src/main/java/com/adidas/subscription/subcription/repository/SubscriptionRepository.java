967ce997138d6ca3c81d62ea01c2d04689130811
Arturo <art2dit2@hotmail.com>
2025-10-28 13:30:27 +0100

    Adding new query to the repository

--- subcription-microservice/src/main/java/com/adidas/subscription/subcription/repository/SubscriptionRepository.java
+++ subcription-microservice/src/main/java/com/adidas/subscription/subcription/repository/SubscriptionRepository.java
@@ -1,8 +1,11 @@
 package com.adidas.subscription.subcription.repository;
 
 import com.adidas.subscription.subcription.entity.SubscriptionEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Long> {
+
+  Optional<SubscriptionEntity> findBySubscriptionId(String uuid);
 
 }
