b397af5131e38dadb4dd3b70ff144bf4df4be4a9
Arturo <art2dit2@hotmail.com>
2025-11-01 17:17:08 +0100

    Deleting unused methods

--- subcription-microservice/src/main/java/com/adidas/subscription/subcription/repository/SubscriptionRepository.java
+++ subcription-microservice/src/main/java/com/adidas/subscription/subcription/repository/SubscriptionRepository.java
@@ -1,13 +1,9 @@
 package com.adidas.subscription.subcription.repository;
 
 import com.adidas.subscription.subcription.entity.SubscriptionEntity;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, UUID> {
-
-  //Optional<SubscriptionEntity> findBySubscriptionId(String uuid);
-  Optional<SubscriptionEntity>findByEmail(String email);
 
 }
